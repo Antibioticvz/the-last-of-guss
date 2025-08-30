8d93926c13b7043dbc09cddacefaa68c4df58e67
Victor Zadorozhnyy <antibioticvz@gmail.com>
2025-08-30 12:57:25 +0400

    fix: standardize quotation marks and add missing commas in vite.config.ts

--- the-last-of-guss-frontend/vite.config.ts
+++ the-last-of-guss-frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,13 +7,13 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true,
-    cors: true
+    cors: true,
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true
+    outDir: "dist",
+    sourcemap: true,
   },
   define: {
-    global: 'globalThis',
-  }
+    global: "globalThis",
+  },
 })
